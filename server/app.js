7eb0ff0d0bae23f5bad96ed441cee04f467e618e
ImhyeMi <jazzyfact2000@gmail.com>
2021-10-21 03:10:16 +0900

    [ADD] 몽고 디비 연결

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 
-const app = express()
+
+const app = express();
+const {MONGO_URI} = config;
+
+
+mongoose.connect(MONGO_URI,{
+        useNeWUrlParser : true,
+        useUnifiedTopology: true,
+})
+
+.then(() => console.log("몽고 디비 연결 성공!"))
+.catch((e) => console.log(e));
 
 app.get('/')
 
