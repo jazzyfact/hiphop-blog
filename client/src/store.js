8227b95fba66d4cc4eccfc300c74de5478da11b7
ImhyeMi <jazzyfact2000@gmail.com>
2021-10-22 22:47:06 +0900

    [ADD] router 초기 셋팅 추가

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,14 @@ import {routerMiddleware} from 'connected-react-router';
 import createRootReducer from './redux/reducers/index';
 import rootSaga from './redux/sagas';
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 const initialSatate = {};
 
 const middlewares = [sagaMiddleware, routerMiddleware(history)];
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION__;
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 //배포시 개발자 도구 숨기기
 const composeEnhancer = process.env.NODE_ENV === "production" ? compose : devtools || compose;
@@ -21,7 +21,7 @@ const composeEnhancer = process.env.NODE_ENV === "production" ? compose : devtoo
 const store = createStore(
     createRootReducer(history),
     initialSatate, //웹의 초기 값
-    composeEnhancer(applyMiddleware(...middlewares)),
+    composeEnhancer(applyMiddleware(...middlewares))
 );
 sagaMiddleware.run(rootSaga);
 
