e899fd2455976e1d05398d4f81963439c9edef2a
ImhyeMi <jazzyfact2000@gmail.com>
2021-10-23 00:19:22 +0900

    [ADD] 헤더 추가

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Collapse, Container, Navbar, NavbarToggler, Nav } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const AppNavbar = () => {
+    return(
+        <>
+        <Navbar color="dark" dark expand="lg" className="sticky-top">
+            <Container>
+                <Link to="/" className="text-white text-decoration-none">
+                    Side Project blog 
+                </Link>
+                <NavbarToggler/>
+                <Collapse isOpen={true} navbar>
+                    <Nav className="ml-auto d-felx justify-content-around" navbar>
+                        {true ? (
+                        <h1 className="text-white"> authLink </h1>
+                        ) : (
+                        <h1 className="text-white"> guestLink </h1>
+                        )}
+                    </Nav>
+                    
+                </Collapse>
+              
+            </Container>
+        </Navbar>
+    </>
+    )
+};
+
+export default AppNavbar;
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import AppNavbar from '../components/AppNavbar';
 
 const MyRouter = () => (
     <>
+        <AppNavbar/>
         <Header/>
             <h1>Hello body</h1>
         <Footer/>
