531b69dba43d5086057c2b83db4c822f666465f4
ImhyeMi <jazzyfact2000@gmail.com>
2021-10-23 00:00:18 +0900

    [ADD] 헤더 이미지 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Row, Col} from 'reactstrap';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div id="page-header" className="mb-3">
+            <Row>
+                <Col md="6" sm="auto" className="text-center m-auto">
+                    <h1>Always Hiphop</h1>
+                    <p>힙합블로그</p>
+                </Col>
+            </Row>
+        </div>
     )
 };
 
